fcb40a32f16e970f989826f13d3256409f398d4e
nabcdelskupriya <90275519+nabcdelskupriya@users.noreply.github.com>
2021-09-07 22:39:52 +0530

    Add files via upload

---
+++ socialapp/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.socialapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    FirebaseAuth auth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                        //    Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            Toast.makeText(MainActivity.this, "Token is missing", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+                        FirebaseDatabase.getInstance().getReference().child(uid).child("token").setValue(token);
+
+                    }
+                });
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNav);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,
+                new Fragment1()).commit();
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNav = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selected = null;
+            switch (item.getItemId()){
+                case R.id.profile_bottom:
+                    selected = new Fragment1();
+                    break;
+
+                case R.id.ask_bottom:
+                    selected = new Fragment2();
+                    break;
+
+                case R.id.queue_bottom:
+                    selected = new Fragment3();
+                    break;
+
+                case R.id.home_bottom:
+                    selected = new Fragment4();
+                    break;
+
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,
+                    selected).commit();
+
+
+            return true;
+
+        }
+    };
+
+
+
+
+}
